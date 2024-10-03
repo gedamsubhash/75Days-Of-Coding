a72af136660403579bb7c3bd2c70190fe29a4744
swaggyhustler <gedamsubhash086@gmail.com>
2024-10-03 23:06:14 +0530

    Primes Problems Added

--- Data Structures/Math/LCM_GCF.py
+++ Data Structures/Math/LCM_GCF.py
@@ -1,3 +1,37 @@
+# def gcd(x, y):
+#     val1,val2=None, None
+#     if x>y:
+#         val1=x
+#         val2=y
+#     else:
+#         val1=y
+#         val2=x
+#     while(val2!=0):
+#         temp=val2
+#         val2=val1%val2
+#         if(val2==0):
+#             print(temp)
+#         val1=temp
+# gcd(98, 56);
+
+# def gcd_recurr(a, b):
+#     if(a==0):
+#         return a
+#     if(b==0):
+#         return b
+#     if(a==b):
+#         return a
+#     if(a>b):
+#         return gcd_recurr(b, a-b)
+#     return gcd_recurr(a, b-a)
+# print(gcd_recurr(98, 56))
+
+# def gcd(a,b):
+#     if b==0:
+#         return a
+#     return gcd(b, a%b)
+# print(gcd(28, 20))
+
 def gcf(a, b):
     if(b==0):
         return a
---
+++ Data Structures/Math/Primes_Problems.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+int gcd(int , int );
+int lcm(int* , int);
+void prime_factors(int);
+void seivesOfEratosthenis(int);
+void test();
+void spf(int );
+int main(){
+    // cout<<gcd(3,7);
+    // int arr[]={2,4,6};
+    // cout<<lcm(arr, 3);
+    // prime_factors(100);
+    // int n;
+    // cin>>n;
+    // seivesOfEratosthenis(n);
+    // test();
+    spf(20); // Smallest Prime Factors - Seives Eratosthenis
+}
+
+int gcd(int a, int b){
+    if(b==0)
+        return a;
+    return gcd(b, a%b);
+}
+
+int lcm(int arr[], int n){
+    int res = arr[0];
+    for(int i=1; i<n; i++){
+        res = (res*arr[i])/gcd(res, arr[i]);
+    }
+    return res;
+}
+
+void prime_factors(int n){
+    while(n%2==0){
+        cout<<2<<" ";
+        n/=2;
+    }
+
+    for(int i=3; i<=sqrt(n); i+=2){
+        while(n%i==0){
+            cout<<i<<" ";
+            n/=i;
+        }
+    }
+
+    if(n>2){
+        cout<<n<<endl;
+    }
+}
+
+void seivesOfEratosthenis(int n){
+    bool arr[n+1];
+    memset(arr, true, sizeof(arr));
+    for(int i=2; i<=sqrt(n); i++){
+        if(arr[i]){
+            for(int j=i*i; j<=n; j+=i){
+                arr[j]=false;
+            }
+        }
+    }
+    for(int k=2; k<=n; k++){
+        if(arr[k])
+            cout<<k<<" ";
+    }
+}
+
+void test(){
+    bool t[10]={true};
+    memset(t, true, sizeof(t));
+    for(bool i: t)
+        cout<<i<<" ";
+}
+
+void spf(int n){
+    int spf[n+1]={0};
+    for(int i=2; i<=n; i++){
+        if(spf[i]==0){
+            for(int j=i; j<=n; j+=i){
+                if(spf[j]==0){
+                    spf[j]=i;
+                }
+            }
+        }
+    }
+
+    for(int i=2; i<=n; i++){
+        cout<<spf[i]<<" ";
+    }
+}
